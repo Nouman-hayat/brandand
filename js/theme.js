6d4347adeaff5860747663811f6c5a59141cdc20
nouman <nomi676767@gmail.com>
2022-11-16 20:00:04 +0500

    first commit

---
+++ js/theme.js
@@ -0,0 +1,28 @@
+
+
+ 
+
+  $(".home-slider").owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 70,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
+		},
+	});
+
+
+ 
